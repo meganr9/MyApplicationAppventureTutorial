d8e4585c169a85fcd8ec9bf4743d60d3afd9dcac
meganr9 <meganreiffer@gmail.com>
2017-02-17 17:26:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/whyth/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.whyth.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void buttonTwoClicked(View view) {
+        textView.setText("I was clicked!!!!!!");
+    }
+}
